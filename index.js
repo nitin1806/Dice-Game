4b8e5d99c30ea204b787e54deff609990020402d
nitin1806 <91219310+nitin1806@users.noreply.github.com>
2022-11-12 03:50:24 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -18,20 +18,23 @@ function rollDice(){
   var randomImage2 = "images/dice"+randomNumber2+".png";
   var image2 = document.querySelectorAll("img")[1];
   image2.setAttribute("src" , randomImage2);
-  $(".dice").fadeOut().fadeIn();
+  //$(".dice").fadeOut().fadeIn();
   if(randomNumber1>randomNumber2){
-    $("h1").fadeOut().slideUp().slideDown();
+  //  $("h1").fadeOut().slideUp().slideDown();
+  $("h1").animate({margin: 20}).animate({opacity: 0.5});
     document.querySelector("h1").innerHTML = "🚩 Player 1 wins";
-    $("h1").fadeIn();
+  //  $("h1").fadeIn();
   }else if (randomNumber1 === randomNumber2) {
-    $("h1").fadeOut().slideUp().slideDown();
+  //  $("h1").fadeOut().slideUp().slideDown();
+  $("h1").animate({margin: 20}).animate({opacity: 0.5});
     document.querySelector("h1").innerHTML = "It's a draw 🤞";
-    $("h1").fadeIn();
+  //  $("h1").fadeIn();
   }
   else{
-    $("h1").fadeOut().slideUp().slideDown();
+  //  $("h1").fadeOut().slideUp().slideDown();
+  $("h1").animate({margin: 20}).animate({opacity: 0.5});
     document.querySelector("h1").innerHTML = "player 2 wins 🚩";
-    $("h1").fadeIn();
+  //  $("h1").fadeIn();
   }
 
 }
